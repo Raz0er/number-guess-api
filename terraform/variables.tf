59f24dda85c9f91493ba27739bc158373329edda
Krzysztof Rosłon <kroslonit@gmail.com>
2026-07-12 20:35:10 +0200

    Translate Terraform variable descriptions to English

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,34 +1,39 @@
 variable "aws_region" {
-  description = "Region AWS dla infrastruktury"
+  description = "AWS region where the infrastructure will be created"
   type        = string
   default     = "eu-central-1"
 }
 
 variable "project_name" {
-  description = "Nazwa projektu używana w nazwach zasobów"
+  description = "Project name used for resource names and tags"
   type        = string
   default     = "number-guess-api"
 }
 
 variable "environment" {
-  description = "Nazwa środowiska"
+  description = "Deployment environment name"
   type        = string
   default     = "production"
 }
 
 variable "instance_type" {
-  description = "Typ instancji EC2"
+  description = "EC2 instance type"
   type        = string
   default     = "t3.micro"
 }
 
 variable "allowed_ssh_cidr" {
-  description = "Publiczny adres IP dopuszczony do SSH, zakończony /32"
+  description = "Public IPv4 CIDR allowed to connect through SSH, for example 1.2.3.4/32"
   type        = string
+
+  validation {
+    condition     = can(cidrnetmask(var.allowed_ssh_cidr))
+    error_message = "allowed_ssh_cidr must be a valid IPv4 CIDR, for example 1.2.3.4/32."
+  }
 }
 
 variable "public_key_path" {
-  description = "Ścieżka do publicznego klucza SSH"
+  description = "Path to the public SSH key"
   type        = string
   default     = "~/.ssh/id_ed25519.pub"
 }
